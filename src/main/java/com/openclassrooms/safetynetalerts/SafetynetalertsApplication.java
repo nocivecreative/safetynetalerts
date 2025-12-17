086fe3a9af4fa5ed06db7bedf602e3246229fa8c
nocivecreative <nocivecreative@gmail.com>
2025-12-17 09:52:41 +0100

    Clean-up main app

--- src/main/java/com/openclassrooms/safetynetalerts/SafetynetalertsApplication.java
+++ src/main/java/com/openclassrooms/safetynetalerts/SafetynetalertsApplication.java
@@ -2,19 +2,12 @@ package com.openclassrooms.safetynetalerts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.openclassrooms.safetynetalerts.model.DataFile;
-import com.openclassrooms.safetynetalerts.repository.JsonDataRepo;
 
 @SpringBootApplication
 public class SafetynetalertsApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SafetynetalertsApplication.class, args);
-		JsonDataRepo repo = ctx.getBean(JsonDataRepo.class);
-		DataFile data = repo.loadData();
-		System.out.println(data);
+		SpringApplication.run(SafetynetalertsApplication.class, args);
 	}
 
 }
